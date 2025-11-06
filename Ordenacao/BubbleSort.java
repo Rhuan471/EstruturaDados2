ebafa347ce01b5889911a26a399984554a831ab1
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-11-06 11:09:09 -0300

    Update BubbleSort.java

--- Ordenacao/BubbleSort.java
+++ Ordenacao/BubbleSort.java
@@ -4,8 +4,8 @@ public class BubbleSort {
 
 		int arr[] = {7,6,4,3};
 
-		for(int i = 0; i < arr.lenght-1; i++) {
-			for(int j = 0; j < arr.lenght-1; j++) 
+		for(int i = 0; i < arr.length-1; i++) {
+			for(int j = 0; j < arr.length-1; j++) 
 
 				if(arr[j] > arr[j+1]) {
 					int temp = arr[j];
@@ -14,7 +14,7 @@ public class BubbleSort {
 				}
 			}
 		}
-		for(int i = 0; i < arr.lenght; i++) {
+		for(int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 	}
 }
