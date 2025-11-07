b2ff1c29e4be7cd633285a18bcb7d59d1bb6596a
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-11-07 10:08:41 -0300

    Update BubbleSort.java

--- Ordenacao/BubbleSort.java
+++ Ordenacao/BubbleSort.java
@@ -1,20 +1,22 @@
 public class BubbleSort {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 
-		int arr[] = {7,6,4,3};
-
-		for(int i = 0; i < arr.length-1; i++) {
-			for(int j = 0; j < arr.length-1; j++) 
-
-				if(arr[j] > arr[j+1]) {
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-				}
-			}
-		}
-		for(int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-	}
+    public static void bubbleSort(int arr[]) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr.length-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
 }
