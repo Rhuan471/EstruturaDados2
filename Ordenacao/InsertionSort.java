f46248ca07afebfe54f23ca5ede4550bf23c4704
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-11-07 12:33:37 -0300

    Update InsertionSort.java

--- Ordenacao/InsertionSort.java
+++ Ordenacao/InsertionSort.java
@@ -1,17 +1,22 @@
 public class InsertionSort {
 
-	public static void main(String[] args) {
-
-		int arr[] = {5,3,8,2,9,1,11,0,4};
-
+	    public static int[] insertionSort(int[] arr) {
+        for(int i = 1; i < arr.length; i++) {
 			for(int j = 1; j > 0; j--) {
 				if(arr[j] < arr[j-1]) {
 					int temp = arr[j];
 					arr[j] = arr[j-1];
 					arr[j-1] = temp;
-					j = arr.length;
-						System.out.println("Troca: " + arr[j] + " com " + arr[j-1]);
 				}
 			}
-	}
+	    }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 6, 3, 4, 8, 7};
+        int[] sortedArr = insertionSort(arr);
+        for(int num : sortedArr) {
+            System.out.print(num + " ");
+        }
+    }
 }
